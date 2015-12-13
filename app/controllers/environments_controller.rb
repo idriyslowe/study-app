10b5dc23e448f8020947d1297f95b5c6bc433c3b
idriyslowe <idriyswins@gmail.com>
2015-12-13 14:52:47 -0600

    created edit, show, new views and methods in the environment controller

--- app/controllers/environments_controller.rb
+++ app/controllers/environments_controller.rb
@@ -3,14 +3,27 @@ class EnvironmentsController < ApplicationController
     @environments = Environment.all
   end
   def new
+    @environments = Environment.new
   end
   def create
+    @environments = Environment.create(
+      name: params[:name], 
+      status: "active", 
+      last_used: 1, 
+      visit_count: 1, 
+      user_id: 1)
+    redirect_to '/environments'
   end
   def show
+    @environments = Environment.find_by(id: params[:id])
   end
   def edit
+    @environments = Environment.find_by(id: params[:id])
   end
   def update
+    environment = Environment.find_by(id: params[:id])
+    environment.update(name: params[:name], status: params[:status])
+    redirect_to '/environments'
   end
   def destroy
   end
