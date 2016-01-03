b775d48de5ae360d7ba5d7c9e30efd4496448eb9
idriyslowe <idriyswins@gmail.com>
2016-01-03 16:25:41 -0600

    pushing before installing angular

--- app/models/note_page.rb
+++ app/models/note_page.rb
@@ -2,4 +2,8 @@ class NotePage < ActiveRecord::Base
   belongs_to :environment
   has_many :bookmarks
   has_many :comments
+
+  def text_sample
+    truncate(text, :length => 10, :separator => ' ')
+  end
 end
