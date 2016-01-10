42b0279e5b64ca408a10e27bd66fa7e12ea635f3
idriyslowe <idriyswins@gmail.com>
2016-01-10 10:44:23 -0600

    saving before switching branches

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,11 +1,13 @@
 class BookmarksController < ApplicationController
   def index
+    @bookmarks = Bookmark.all
   end
   def new
   end
   def create
   end
   def show
+    @bookmark = Bookmark.find_by(id: params[:id])
   end
   def edit
   end
