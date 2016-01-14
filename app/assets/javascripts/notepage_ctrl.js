f42a015c8ba5f4f4809062226ca5e6d3d7f60f95
idriyslowe <idriyswins@gmail.com>
2016-01-13 19:21:17 -0600

    got current note attributes to show in placeholders in angular fields to prove it KNOWS

--- app/assets/javascripts/notepage_ctrl.js
+++ app/assets/javascripts/notepage_ctrl.js
@@ -2,11 +2,22 @@
 
 (function() {
   angular.module("app").controller("notepage", function($scope, $http, $timeout) {
+    $scope.setup = function() {
+      $http.get('/notes.json').then(function(response) {
+        $scope.notes = response.data;
+      });
+    };
 
     $scope.noteText = {};
       var timeout = null;
       var saveUpdates = function() {
-        // .... save data to server
+          $http.post('/notes.json', noteText).then(function(response) {
+            console.log(response);
+            $scope.incidents.push(noteText);
+          }, function(error) {
+            console.log(error);
+            $scope.errors = error.data.errors;
+          });
       };
       var debounceSaveUpdates = function(newVal, oldVal) {
         if (newVal != oldVal) {
