5aaf13e15193d17620b251720895ed9d1693ffcc
idriyslowe <idriyswins@gmail.com>
2016-01-14 19:15:10 -0600

    mid-way through rebuilding notepage show. editNote doesn't work and the object attributes aren't showing

--- app/assets/javascripts/notepage_ctrl.js
+++ app/assets/javascripts/notepage_ctrl.js
@@ -1,8 +1,5 @@
 /* global angular */
 
-(function() {
-  angular.module("app").controller("notepage", function($scope, $http) {
-
     // $scope.autoSave = setInterval(function() {
     //   var form = $('#text');
     //   var method = form.attr('patch').toLowerCase();
@@ -11,11 +8,56 @@
     //   });
     // },5000);
 
+// WHAT TO DO ABOUT THIS RESPONSE. HOW TO GET RESPONSE FROM UPDATE?
+
+(function() {
+  angular.module("app").controller("notes", function($scope, $http) {
+    $scope.setup = function() {
+      $http.get('/api/notes.json').then(function(response) {
+        $scope.notes = response.data;
+      });
+    };
+
     $scope.clickedTextArea = function(textArea) {
       console.log(textArea);
     };
 
-    window.$scope = $scope;
+    $scope.newNote = function(inputNotePageId) {
+      var note = {
+        'text': null,
+        'note_page_id': inputNotePageId
+      };
+      $http.post('/api/notes.json', note).then(function(response) {
+        console.log(response);
+        $scope.note.push(note);
+        
+      }, function(error) {
+        console.log(error);
+        $scope.errors = error.data.errors;
+      });
+    };
 
+    $scope.editNote = function(inputNoteObject) {
+      var note = {
+        'id': inputNoteObject.id,
+        'text': inputNoteObject.text,
+        'note_page_id': inputNoteObject.note_page_id
+      };
+      $http.patch('/api/notes/:id.json', note).then(function(response) {
+        console.log(response);
+        $scope.note.push(note);
+        
+      }, function(error) {
+        console.log(error);
+        $scope.errors = error.data.errors;
+      });
+    };
+
+    $scope.deleteNote = function(inputText) {
+      var index = $scope.people.indexOf(inputText);
+      $scope.people.splice(index, 1);
+    };
+
+    window.$scope = $scope;
   });
 })();
--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -7,11 +7,13 @@ class Api::NotesController < ApplicationController
     note_page = NotePage.find_by(id: params[:note_page_id])
   end
   def create
-    @note = Note.create(
+    @note = Note.new(
       text: nil,
       note_page_id: params[:note_page_id],
       )
-    redirect_to "/note_pages/#{@note.note_page_id}"
+    if @note.save
+      render json: { errors: @note.errors.full_messages }, status: 422
+    end
   end
   def show
   end
@@ -21,11 +23,9 @@ class Api::NotesController < ApplicationController
       text: params[:text],
       note_page_id: params[:note_page_id]
       )
-    redirect_to "/note_pages/#{@note.note_page_id}"
   end
   def destroy
     note = Note.find_by(id: params[:id])
     note.destroy
-    redirect_to "/note_pages/#{note.id}"
   end
 end
