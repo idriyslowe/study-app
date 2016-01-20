87d4fdd0e1d5951fe1bb8e7750d37db99130cc4d
idriyslowe <idriyswins@gmail.com>
2016-01-20 17:33:40 -0600

    app is finally receiving screenshot data and saving it to public folder

--- app/controllers/api/bookmarks_controller.rb
+++ app/controllers/api/bookmarks_controller.rb
@@ -2,30 +2,40 @@ class Api::BookmarksController < ApplicationController
   def index
     @bookmarks = Bookmark.all
   end
+
   def new
   end
+  
   def create
+    data = params[:screenshot_data]
+    filename = "#{SecureRandom.urlsafe_base64}.png" 
+    File.open(Rails.root.join('public', filename), 'wb') do |f|
+      f.write(Base64.decode64(data['data:image/png;base64,'.length .. -1]))
+    end
     @bookmark = Bookmark.new(
       name: "From Extension",
       path: params[:path],
       note_id: params[:note_id],
-      screenshot_data: params[:screenshot_data],
-      user_id: current_user.id
+      user_id: current_user.id,
+      screenshot_data: filename
       )
-    # @incoming_file = params[:screenshot_data]
-    # file_name = current_user.handle + "#{DateTime.now}"
-    # FileUtils.mv @incoming_file.tempfile, "/Users/igrant/acltc/study-app/public/png/#{file_name}"
     if @bookmark.save
+      render json: { message: "success"}
+    else
       render json: { errors: @bookmark.full_messages }, status: 422
     end
   end
+  
   def show
     @bookmark = Bookmark.find_by(id: params[:id])
   end
+  
   def edit
   end
+  
   def update
   end
+  
   def destroy
   end
 end
