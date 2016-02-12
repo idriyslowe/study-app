01f2ff23c48e24e18fe5d44785e96a79501013a2
idriyslowe <idriyswins@gmail.com>
2016-02-12 12:20:50 -0600

    got local postgres server hooked up

--- db/schema.rb
+++ db/schema.rb
@@ -1,67 +1,78 @@
 # encoding: UTF-8
-# This file is auto-generated from the current state of the database.If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations from scratch.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160120025519) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookmarks", force: :cascade do |t|
-    t.string   "name",            limit: 255
-    t.string   "path",            limit: 255
-    t.integer  "note_id",         limit: 4
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.text     "screenshot_data", limit: 65535
-    t.integer  "user_id",         limit: 4
+    t.string   "name"
+    t.string   "path"
+    t.integer  "note_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.text     "screenshot_data"
+    t.integer  "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
-    t.integer  "user_id",      limit: 4
-    t.string   "text",         limit: 255
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "note_page_id", limit: 4
+    t.integer  "user_id"
+    t.string   "text"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "note_page_id"
   end
 
   create_table "environments", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.string   "status",      limit: 255
-    t.integer  "visit_count", limit: 4
-    t.integer  "user_id",     limit: 4
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.string   "image_path",  limit: 255
+    t.string   "name"
+    t.string   "status"
+    t.integer  "visit_count"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.boolean  "sharing"
   end
 
   create_table "note_pages", force: :cascade do |t|
-    t.string   "title",          limit: 255
-    t.integer  "environment_id", limit: 4
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.string   "summary",        limit: 255
+    t.string   "title"
+    t.integer  "environment_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "summary"
   end
 
   create_table "notes", force: :cascade do |t|
-    t.text     "text",         limit: 65535
-    t.integer  "note_page_id", limit: 4
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.text     "text"
+    t.integer  "note_page_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.string   "handle",                 limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "handle"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
