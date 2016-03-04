d93c3e354f306054b862fe3af6ce97619c95349d
idriyslowe <idriyswins@gmail.com>
2016-03-04 17:44:23 -0600

    unassigned bookmarks section of notepage only shows unassigned bookmarks from that user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :environments
   has_many :bookmarks
+
+  def unassigned_bookmarks
+    bookmarks.where(note_id: nil)
+  end
 end
